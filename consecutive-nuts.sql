e373bb435f93ef38721b86484964320008cc6ec8
Shayan Balaee <shayanfall79@gmail.com>
2022-04-21 18:57:58 +0430

    Create consecutive-nuts.sql

---
+++ consecutive-nuts.sql
@@ -0,0 +1,13 @@
+select distinct l1.Num As ConsecutiveNums from Logs l1, Logs l2, Logs l3
+where
+l1.id = l2.id - 1
+and l2.id = l3.id - 1
+and l1.num = l2.num
+and l2.num = l3.num
+
+#if the id's aren't consecutive we can order by the desired column and index the rows using something similar to this:
+
+SELECT @n := @n + 1 n,
+       num, 
+  FROM table, (SELECT @n := 0) m
+  ORDER BY created_at
