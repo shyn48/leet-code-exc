c3967ad95e98504ce23b072dfd932b3a7c4ab179
Shayan Balaee <shayanfall79@gmail.com>
2022-04-25 23:28:21 +0430

    Update top3salaries.sql

--- top3salaries.sql
+++ top3salaries.sql
@@ -4,3 +4,14 @@ WHERE e.DepartmentId = d.Id
 AND e2.DepartmentId = e1.DepartmentId
 AND e2.Salary >= e1.Salary 
 GROUP BY e1.Id HAVING COUNT(DISTINCT e2.Salary) <= 3;
+
+# faster solution 
+
+select d.Name as 'Department', e1.Name as 'Employee', e1.Salary from Employee e1
+join
+Department d on e1.DepartmentId = d.id
+where (
+    select count(distinct e2.Salary)
+    from Employee e2
+    where e2.Salary > e1.Salary and e1.DepartmentId = e2.DepartmentId
+) < 3
