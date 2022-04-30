22b876840ddfe44448fee25f2007358db3d4cea0
Shayan Balaee <shayanfall79@gmail.com>
2022-04-30 18:35:39 +0430

    Create 262.trips&users.sql

---
+++ 262.trips&users.sql
@@ -0,0 +1,9 @@
+select request_at as day,
+round(sum(case when status != 'completed' then 1 else 0 end)/count(*), 2) 
+as 'cancellation rate' 
+from trips t join users rider on t.client_id = rider.users_id 
+join users driver on t.driver_id = driver.users_id
+where t.request_at between '2013-10-01' and '2013-10-03'
+and rider.banned = 'No'
+and driver.banned = 'No'
+group by request_at
