985f5b06ef37e3d717484dc57cb447613ba890eb
Shayan Balaee <shayanfall79@gmail.com>
2022-05-13 15:38:13 +0430

    Create 607-salesperson.sql

---
+++ 607-salesperson.sql
@@ -0,0 +1,26 @@
+# Write your MySQL query statement below
+
+select s.name from SalesPerson s
+where s.name not in (
+    select s1.name from Orders o join Company c on o.com_id = c.com_id
+join SalesPerson s1 on s1.sales_id = o.sales_id 
+    where c.name = "RED"
+)
+
+
+# alternate
+
+SELECT
+    s.name
+FROM
+    salesperson s
+WHERE
+    s.sales_id NOT IN (SELECT
+            o.sales_id
+        FROM
+            orders o
+                LEFT JOIN
+            company c ON o.com_id = c.com_id
+        WHERE
+            c.name = 'RED')
+;
