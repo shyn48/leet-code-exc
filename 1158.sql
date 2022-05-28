1a947a6dd1cc96449f062dc21dc2aa5156fa7f2e
Shayan Balaee <shayanfall79@gmail.com>
2022-05-28 11:24:21 +0430

    Create 1158.sql

---
+++ 1158.sql
@@ -0,0 +1,5 @@
+select user_id as buyer_id, join_date, count(order_id) as orders_in_2019
+from Users u left join Orders o on u.user_id = o.buyer_id 
+and YEAR(order_date) = '2019'
+group by user_id
+order by user_id
