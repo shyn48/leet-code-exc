0595445148f524f2f406b1f28d240e8347fd2afe
Shayan Balaee <shayanfall79@gmail.com>
2022-06-12 16:22:39 +0430

    Create 23.go

---
+++ 23.go
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    resultHead :=  &ListNode{
+        Val: 0,
+        Next: nil,
+    }
+    
+    temp := resultHead
+    
+    for list1 != nil && list2 != nil {
+        if list1.Val < list2.Val {
+            temp.Next = list1
+            list1 = list1.Next
+        } else {
+            temp.Next = list2
+            list2 = list2.Next
+        }
+        
+        temp = temp.Next
+    }
+    
+    if list1 != nil {
+        temp.Next = list1
+    }
+    
+    if list2 != nil {
+        temp.Next = list2
+    }
+    
+    return resultHead.Next
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+    if len(lists) == 0 {
+        return nil
+    }
+    
+    for len(lists) > 1 {
+        lists = append(lists, mergeTwoLists(lists[0], lists[1]))
+        lists = lists[2:]
+    }
+    
+    return lists[0]
+}
