63a143b9e89dc2962f9f136cca893efcfbbdbe5a
Shayan Balaee <shayanfall79@gmail.com>
2022-06-27 19:57:37 +0430

    Update combineTables.sql

--- combineTables.sql
+++ combineTables.sql
@@ -1,3 +1,7 @@
 select FirstName, LastName, City, State from Person left join Address on Person.PersonId = Address.PersonId;
 
 # pretty easy question
+
+# 2nd run:
+select firstName, lastName, city, state
+from Person left join Address on Person.personId = Address.personId
