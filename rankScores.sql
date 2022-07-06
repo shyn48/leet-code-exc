47fd51b8591e45d5fb3b2f1b62c898ba385e8745
Shayan Balaee <shayanfall79@gmail.com>
2022-07-06 18:26:13 +0430

    Update rankScores.sql

--- rankScores.sql
+++ rankScores.sql
@@ -5,3 +5,10 @@ SELECT
         WHERE s >= Score) as 'rank'
 FROM Scores
 ORDER BY Score desc
+
+# second try
+Select 
+    s.Score,
+    (select count(distinct score) from Scores s2 where s2.Score >= s.Score) as 'Rank'
+    from Scores s
+Order by s.Score desc
