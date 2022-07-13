2a70517c7b0ce33c32c609f54c7ea73c753d73f0
Shayan Balaee <shayanfall79@gmail.com>
2022-07-13 16:47:01 +0430

    Update employeesWithHigherSalaryThanManagers.sql

--- employeesWithHigherSalaryThanManagers.sql
+++ employeesWithHigherSalaryThanManagers.sql
@@ -1,1 +1,12 @@
 select e1.name as 'Employee' from Employee e1 join Employee e2  on e1.managerId = e2.id where e1.salary > e2.salary
+
+#second try
+
+select e1.name as 'Employee'
+from Employee e1, Employee e2 
+    where e1.salary > e2.salary and e1.managerId = e2.Id
+    
+#faster solution
+select a.Name
+from Employee a inner join Employee b on a.ManagerId=b.Id
+where a.Salary>b.Salary
