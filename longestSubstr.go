dc9c8c1831384e3c865cf490761a07bbb1da5d9c
Shayan Balaee <shayanfall79@gmail.com>
2022-07-13 16:40:18 +0430

    Update longestSubstr.go

--- longestSubstr.go
+++ longestSubstr.go
@@ -26,3 +26,33 @@ func lengthOfLongestSubstring(s string) int {
 func main() {
 	println(lengthOfLongestSubstring("tmmzuxt"))
 }
+
+
+# second try
+
+func max(a int, b int) int {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+func lengthOfLongestSubstring2(s string) int {
+    seen := make(map[string]int)
+    
+    maxSubStrLen := 0
+    lastSubStrStartIndex := 0
+    for i := 0; i < len(s);i++ {
+        char := string(s[i])
+        
+        if _, ok := seen[char]; ok {
+            lastSubStrStartIndex = max(seen[char] + 1, lastSubStrStartIndex)
+        }
+        
+        seen[char] = i
+        maxSubStrLen = max(maxSubStrLen, i - lastSubStrStartIndex + 1)
+    }
+    
+    return maxSubStrLen
+}
