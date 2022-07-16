e1b8674f0465101ae2fd9fcee79461fe41aa3937
Shayan Balaee <shayanfall79@gmail.com>
2022-07-16 20:47:38 +0430

    Update customerswithoutorder.sql

--- customerswithoutorder.sql
+++ customerswithoutorder.sql
@@ -2,3 +2,9 @@ select name as 'Customers' from Customers
 where Customers.id not in (
     select customerid from orders
 );
+
+#SECOND TRY
+select name AS 'Customers'
+from Customers c left join Orders o
+on c.id = o.customerId
+where o.customerId is null
