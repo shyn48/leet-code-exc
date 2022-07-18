71dbf2ee070a276bbcb05cb8b1a9f5a9a7f7ac09
Shayan Balaee <shayanfall79@gmail.com>
2022-07-18 19:55:18 +0430

    Update 184.sql

--- 184.sql
+++ 184.sql
@@ -6,3 +6,15 @@ from Employee e join Department as d on e.departmentId = d.Id
         group by departmentId
     ) as maxsalary
     on e.Salary = maxsalary.Salary and e.departmentId = maxsalary.departmentId
+
+
+#second try
+
+select d.name as "Department", e.name as "Employee", e.Salary
+from Employee e 
+join Department d on e.departmentId = d.id 
+where (e.DepartmentId, e.Salary) in (
+    select d1.id, max(salary) from  Employee e1
+    join Department d1 on e1.departmentId = d1.id
+    group by d1.id
+)
