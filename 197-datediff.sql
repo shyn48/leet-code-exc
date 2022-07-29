7a0d00b247eaa82a64d2da66cdf6d6464d4b25e4
Shayan Balaee <shayanfall79@gmail.com>
2022-07-29 18:33:45 +0430

    Update 197-datediff.sql

--- 197-datediff.sql
+++ 197-datediff.sql
@@ -1,3 +1,8 @@
 select w1.id as 'Id' from Weather w1 join Weather w2 
 where DATEDIFF(w1.recordDate, w2.recordDate) = 1
 and w1.temperature > w2.temperature 
+
+#second try
+select w2.id
+from Weather w1, Weather w2 
+where datediff(w2.recordDate, w1.recordDate) = 1 and w2.temperature > w1.temperature
