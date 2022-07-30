d9135b9674d20b63dd4b47f2a878f7195b065c97
Shayan Balaee <shayanfall79@gmail.com>
2022-07-30 15:12:04 +0430

    Update 262.trips&users.sql

--- 262.trips&users.sql
+++ 262.trips&users.sql
@@ -7,3 +7,15 @@ where t.request_at between '2013-10-01' and '2013-10-03'
 and rider.banned = 'No'
 and driver.banned = 'No'
 group by request_at
+
+#second try
+
+select request_at as 'Day',
+round(sum(case when status != 'completed' then 1 else 0 end)/ count(*), 2) as 'Cancellation Rate'
+from trips t join users c on t.client_id = c.users_id
+join users d on t.driver_id = d.users_id 
+where c.banned = 'No' 
+and
+d.banned = 'No'
+and t.request_at between '2013-10-01' and '2013-10-03'
+group by request_at
