a85353d36aa7f1f13392ab06f3cee938f8d9ab61
Shayan Balaee <shayanfall79@gmail.com>
2022-08-16 11:57:54 +0430

    Update 601.sql

--- 601.sql
+++ 601.sql
@@ -10,3 +10,16 @@ and
     (t3.id - t2.id = 1 and t2.id - t1.id =1 and t3.id - t1.id = 2) 
 )
 order by t1.id
+
+#second try
+
+select distinct r.id, r.visit_date, r.people
+from stadium r,
+(select a.id as FROM_ID, a.id+2 as TO_ID
+from stadium a, stadium b, stadium c
+where a.id+1 = b.id
+and b.id+1 = c.id
+and a.people >= 100
+and b.people >= 100
+and c.people >= 100) b
+where r.id between b.FROM_ID and b.TO_ID
