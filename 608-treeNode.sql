2bb5311556b0e83c6eaef5650f81f124f3fbcea2
Shayan Balaee <shayanfall79@gmail.com>
2022-08-23 19:35:09 +0430

    Update 608-treeNode.sql

--- 608-treeNode.sql
+++ 608-treeNode.sql
@@ -20,3 +20,19 @@ id in (
     select distinct p_id from Tree where
     p_id is not null
 ) and p_id is not null 
+
+#alternative 
+
+SELECT
+    id AS `Id`,
+    CASE
+        WHEN tree.id = (SELECT atree.id FROM tree atree WHERE atree.p_id IS NULL)
+          THEN 'Root'
+        WHEN tree.id IN (SELECT atree.p_id FROM tree atree)
+          THEN 'Inner'
+        ELSE 'Leaf'
+    END AS Type
+FROM
+    tree
+ORDER BY `Id`
+;
