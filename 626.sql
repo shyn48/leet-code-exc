b23e6012720f8b9df5bb9b4b1aab33b8b08a819c
Shayan Balaee <shayanfall79@gmail.com>
2022-08-27 19:46:05 +0430

    Update 626.sql

--- 626.sql
+++ 626.sql
@@ -11,3 +11,20 @@ select
     from seat,
     (select count(*) as countId from seat) as seat_count 
     order by id asc
+
+# second try
+
+with cte as (
+    select count(*) as countId from seat
+)
+
+select 
+ (case
+        when id % 2 != 0 and countId != id then id + 1
+        when id % 2 != 0 and countId = id then id
+        else id - 1
+        end
+ ) as id,
+student
+from seat, cte as seat_count
+order by id asc
