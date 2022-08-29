5d036898e033f270be12ca55b101491e6332bf42
Shayan Balaee <shayanfall79@gmail.com>
2022-08-29 16:29:16 +0430

    Update 1050.sql

--- 1050.sql
+++ 1050.sql
@@ -1,3 +1,9 @@
 select actor_id, director_id from ActorDirector
 group by actor_id, director_id 
 having count(*) >= 3 
+
+#second try
+select actor_id, director_id 
+from ActorDirector 
+group by actor_id, director_id
+having count(*) >= 3
