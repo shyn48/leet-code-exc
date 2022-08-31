aa3520c75ec1f036f35d8215479bc5f342bc146d
Shayan Balaee <shayanfall79@gmail.com>
2022-08-31 13:50:10 +0430

    Update 1084.sql

--- 1084.sql
+++ 1084.sql
@@ -7,3 +7,9 @@ where product_id in (
     group by product_id
     HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31'
 )
+
+#second try
+select s.product_id, product_name 
+from Sales s join Product p on s.product_id = p.product_id
+group by s.product_id having min(s.sale_date) >= '2019-01-01' 
+and max(s.sale_date) <= '2019-03-31'
