7678f09197da5ffc11de388c19de95b5b2260e83
Shayan Balaee <shayanfall79@gmail.com>
2022-09-05 18:53:03 +0430

    Update 1148.sql

--- 1148.sql
+++ 1148.sql
@@ -2,3 +2,9 @@ SELECT author_id AS id FROM Views
 where author_id = viewer_id 
 GROUP BY id
 ORDER BY id
+
+# second try
+select distinct(author_id) as id
+from Views
+where author_id = viewer_id
+order by id asc
