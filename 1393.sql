16d56826a0f2588aad44a25cc395f9e605ecce43
Shayan Balaee <shayanfall79@gmail.com>
2022-09-10 19:50:17 +0430

    Update 1393.sql

--- 1393.sql
+++ 1393.sql
@@ -2,3 +2,11 @@ select stock_name, SUM(
     if(operation = 'Buy', -price, price)
 ) as capital_gain_loss
 from Stocks group by stock_name
+
+select 
+stock_name, 
+sum(case when operation = 'Buy'
+then -price
+else price
+end) as capital_gain_loss
+from Stocks group by stock_name
