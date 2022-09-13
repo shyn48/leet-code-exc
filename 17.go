fb0d4a679bb425eab5531024948e5a6ba4dd4857
Shayan Balaee <shayanfall79@gmail.com>
2022-09-13 15:53:13 +0430

    Update 17.go

--- 17.go
+++ 17.go
@@ -37,3 +37,47 @@ func letterCombinations(digits string) []string {
     
     return result
 }
+
+#second attempt
+
+func letterCombinations(digits string) []string {
+    if len(digits) <= 0 {
+        return []string{}
+    }
+    
+    
+    
+   indexedLetters := []string{
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz",
+    }
+    
+    
+    result := []string{""}
+    
+    for _, digit := range digits {
+        strDigit := string(digit)
+        intDigit, _ := strconv.Atoi(strDigit)
+
+        possibleCharacters := indexedLetters[intDigit]
+        
+        var newResult []string
+        for i, _ := range possibleCharacters {
+            for _, currentStr := range result {
+                newResult = append(newResult, string(currentStr) + string(possibleCharacters[i]))
+            }
+        } 
+        
+        result = newResult
+    }
+    
+    return result
+}
