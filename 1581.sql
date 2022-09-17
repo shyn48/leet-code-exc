5b9016a8f2a9755b3b2e06d37cc44fd03ddcadb8
Shayan Balaee <shayanfall79@gmail.com>
2022-09-17 19:16:26 +0430

    Update 1581.sql

--- 1581.sql
+++ 1581.sql
@@ -12,3 +12,11 @@ FROM
 WHERE 
 	Transactions.visit_id IS NULL
 GROUP BY customer_id
+
+
+#second try
+
+select customer_id, count(customer_id) as count_no_trans
+from Visits v left join Transactions t on v.visit_id = t.visit_id
+where transaction_id is null
+group by customer_id
