236eea5bf1c22cfc2a87e99ea1306becd04b40c8
Shayan Balaee <shayanfall79@gmail.com>
2022-09-19 20:44:23 +0430

    Update 1587.sql

--- 1587.sql
+++ 1587.sql
@@ -4,3 +4,12 @@ select name, sum(amount) as balance from
 Users u join Transactions t on u.account = t.account
 group by u.account
 having sum(amount) > 10000
+
+
+#second attempt
+select name, sum(amount) as balance
+from Users u
+     Join Transactions t
+     on u.account = t.account
+group by u.account 
+having balance > 10000
