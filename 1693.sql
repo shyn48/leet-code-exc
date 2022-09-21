09483d1dab0d39142ac5d9a06875aa1751e3debc
Shayan Balaee <shayanfall79@gmail.com>
2022-09-21 19:31:39 +0430

    Update 1693.sql

--- 1693.sql
+++ 1693.sql
@@ -3,3 +3,8 @@ select date_id, make_name, count(distinct lead_id) as unique_leads,
 count(distinct partner_id) as unique_partners
 from DailySales 
 group by date_id, make_name
+
+#second attempt
+select date_id, make_name, count(distinct lead_id) as unique_leads, count(distinct partner_id) as unique_partners
+from DailySales
+group by date_id, make_name
